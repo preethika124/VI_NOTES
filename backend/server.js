9dc3b7f3db409b41c37b70f47667e2fd1e4d3a4c
preethika <preethikapenta@gmail.com>
2026-03-28 23:25:06 +0530

    updated backend url

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: "*"
+  origin: "https://vi-notes-frontend-x0i6.onrender.com"
 }));
 const PORT=process.env.PORT || 5000;
 
